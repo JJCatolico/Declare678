d9d889e20b617a64056d459444f213739f03d366
JJCatolico <heavyweightj@gmail.com>
2017-07-28 13:55:20 -0400

    Declare678

---
+++ Declare678.js
@@ -0,0 +1,44 @@
+
+//problem 6
+function myName(name){
+  alert("Your is " + name)
+
+  
+  
+}
+
+//problem 7
+
+function Dogs(){
+}
+
+console.log("I love dogs")
+
+// problem 8
+
+function doorWay(door){
+ 
+  if (door === 1)
+ 
+  {
+    alert("You win!");
+  }
+  
+  if (door === 2)
+  
+  {
+    alert("You won!");
+  }
+ 
+  else if (door === 3)
+ 
+  {
+    alert("You win-won!");
+    
+  }
+  
+  else 
+    return (false);
+  
+
+}
